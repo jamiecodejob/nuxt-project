49e05c9a71fe14f636f795fdc621d24e892d6224
Chen Ran Wong <>
2025-10-17 21:22:20 +0800

    unit test

---
+++ app/components/AppNumber.test.ts
@@ -0,0 +1,15 @@
+// @vitest-environment nuxt
+import {it, expect, describe } from 'vitest';
+import {mountSuspended} from '@nuxt/test-utils/runtime';
+import AppNumber from './AppNumber.vue';
+
+describe('AppNumber', ()=> {
+    it('can mount the component', async()=>{
+        const component = await mountSuspended(AppNumber)
+        expect(component.html()).toContain('Number')
+    })
+    it('can mount the component', async()=>{
+        const component = await mountSuspended(AppNumber)
+        expect(component.html()).not.toContain('String')
+    })
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app:{
+    head: {
+      htmlAttrs: {
+        style: 'background-color:#111;color: #f0f0f0'            }
+    }
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
