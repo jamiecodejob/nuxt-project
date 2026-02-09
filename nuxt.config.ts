cd65eb9a96b77dbf979b52a7a6a2a8f1b1d93b3a
Chen Ran Wong <>
2026-02-09 22:20:28 +0800

    eslint setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  app:{
-    head: {
-      htmlAttrs: {
-        style: 'background-color:#111;color: #f0f0f0'            }
-    }
-  },
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
-})
+    modules: [
+        "@nuxt/content",
+        "@nuxt/eslint",
+        "@nuxt/image",
+        "@nuxt/scripts",
+        "@nuxt/test-utils",
+        "@nuxt/ui",
+    ],
+    devtools: { enabled: true },
+    app: {
+        head: {
+            htmlAttrs: {
+                style: "background-color:#111;color: #f0f0f0" },
+        },
+    },
+    compatibilityDate: "2025-07-15",
+    eslint: {
+        config: {
+            stylistic: {
+                semi: true,
+                quotes: "double",
+                commaDangle: "always-multiline",
+                indent: 4,
+            },
+        },
+    },
+});
