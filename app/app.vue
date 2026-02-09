9a481ee3f18e64f71c6551f183f3afa0caa290ad
Chen Ran Wong <>
2026-02-09 22:19:55 +0800

    nuxt test

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <AppNumber />
-  </div>
+    <div>
+        <AppNumber />
+    </div>
 </template>
--- app/components/AppNumber.test.ts
+++ app/components/AppNumber.test.ts
@@ -1,15 +1,15 @@
 // @vitest-environment nuxt
-import {it, expect, describe } from 'vitest';
-import {mountSuspended} from '@nuxt/test-utils/runtime';
-import AppNumber from './AppNumber.vue';
+import { it, expect, describe } from "vitest";
+import { mountSuspended } from "@nuxt/test-utils/runtime";
+import AppNumber from "./AppNumber.vue";
 
-describe('AppNumber', ()=> {
-    it('can mount the component', async()=>{
-        const component = await mountSuspended(AppNumber)
-        expect(component.html()).toContain('Number')
-    })
-    it('can mount the component', async()=>{
-        const component = await mountSuspended(AppNumber)
-        expect(component.html()).not.toContain('String')
-    })
-})
+describe("AppNumber", () => {
+    it("can mount the component", async () => {
+        const component = await mountSuspended(AppNumber);
+        expect(component.html()).toContain("Number");
+    });
+    it("can mount the component", async () => {
+        const component = await mountSuspended(AppNumber);
+        expect(component.html()).not.toContain("String");
+    });
+});
