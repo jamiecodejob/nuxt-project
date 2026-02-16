376ca7e8ed974f1358340b9179cf07111c44cf26
Chen Ran Wong <>
2026-02-17 00:40:48 +0800

    nuxtImg

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 </script>
 <template>
-    <div>
-        hello world
+    <div class="flex justify-center items-center ">
+        <span class="mr-4">coffee shop</span>
+       <NuxtImg src="/coffee-shop.jpg" alt="coffee shop" width="300" densities="x1" />
     </div>
 </template>
 <style scoped>
