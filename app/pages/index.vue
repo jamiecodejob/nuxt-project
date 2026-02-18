ddc2e52ae2ad582798ad7a3d5fa418e903626e51
Chen Ran Wong <>
2026-02-18 18:46:19 +0800

    fix style

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -27,7 +27,7 @@ const { data, error } = await useFetch<RecipeResponse>("https://dummyjson.com/re
                     </div>
                     </div>
                     <button
-                    class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-base lg:text-lg cursor-pointer"
+                    class="px-4 py-2 text-white self-start bg-yellow-400 rounded-md text-base lg:text-lg cursor-pointer"
                     >
                     View
                     </button>
